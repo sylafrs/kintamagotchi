d957f5fdd8cc3466b6d307d6a814459cb24d43dd
f.settama <f.settama@isartdigital.com>
2015-02-07 11:43:55 +0100

    Panel shop Add

---
+++ UnityProject/Assets/Kintamagotchi/Scripts/UI/ItemUIShopValue.cs
@@ -0,0 +1,19 @@
+﻿//******************************************************************************
+// Authors: Frederic SETTAMA  
+//******************************************************************************
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+//******************************************************************************
+public class ItemUIShopValue : MonoBehaviour 
+{
+#region Script Parameters
+	public Text		Name;
+	public Text		Price;
+	public Text		Desc;
+	public Image	Sprite;
+
+#endregion
+}
---
+++ UnityProject/Assets/Kintamagotchi/Scripts/UI/PanelGenerator.cs
@@ -0,0 +1,116 @@
+﻿//******************************************************************************
+// Authors: Frederic SETTAMA  
+//******************************************************************************
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+//******************************************************************************
+
+public enum TypePanel
+{
+	Shop,
+	Inventory
+}
+
+public class PanelGenerator : MonoBehaviour 
+{
+#region Script Parameters
+	public List<Canvas> ItemsList;
+	public ItemShop		ItemsShop;
+	public TypePanel	Type;
+#endregion
+
+#region Properties
+#endregion
+
+#region Fields
+	// Const -------------------------------------------------------------------
+
+	// Private -----------------------------------------------------------------
+#endregion
+
+#region Unity Methods
+	void OnEnable()
+	{
+		if (ItemsList.Count == 0 || Type == TypePanel.Inventory)
+		{
+			GeneratePanel();
+		}
+	}
+#endregion
+
+#region Methods
+	public void Clear()
+	{
+		foreach (Transform child in transform)
+		{
+			Destroy(child.gameObject);
+		}
+		transform.DetachChildren();
+	}
+#endregion
+
+#region Implementation
+	private void GeneratePanel()
+	{
+		Clear();
+		if (Type == TypePanel.Inventory)
+			GenerateInventoryPanel();
+		else
+			GenerateShopPanel();
+	}
+	
+	private void GenerateShopPanel()
+	{
+		foreach(ItemDesc item in ItemsShop.ItemDescList)
+		{
+			var container = GameObject.Instantiate(Resources.Load("Prefabs/UI/" + Type)) as GameObject;
+			SetParent(container, transform);
+			var uiValue = container.GetComponent<ItemUIShopValue>();
+			if (!uiValue)
+			{
+				Debug.LogError("no ItemUIValue on prefab " + container.name);
+				return;
+			}
+			uiValue.Name.text = item.Name;
+			uiValue.Price.text = item.Price.ToString();
+			uiValue.Desc.text = item.Desc;
+		}
+	}
+	private void GenerateInventoryPanel()
+	{
+		foreach(var item in GameData.Get.Data.Inventory)
+		{
+			var container = GameObject.Instantiate(Resources.Load("Prefabs/UI/" + Type)) as GameObject;
+			SetParent(container, transform);
+			var uiValue = container.GetComponent<ItemUIInventoryValue>();
+			if (!uiValue)
+			{
+				Debug.LogError("no ItemUIValue on prefab " + container.name);
+				return;
+			}
+			uiValue.Name.text = item.ItemDetail.Name;
+			uiValue.Count.text = item.Number.ToString();
+			uiValue.Desc.text = item.ItemDetail.Desc;
+		}
+	}
+
+	void SetParent(GameObject obj, Transform parent)
+	{
+		Vector3 position;
+		Quaternion rotation;
+		Vector3 scale;
+
+		position = obj.transform.position;
+		rotation = obj.transform.rotation;
+		scale = obj.transform.localScale;
+		obj.transform.parent = parent;
+		obj.transform.localPosition = position;
+		obj.transform.localRotation = rotation;
+		obj.transform.localScale = scale;
+	}
+#endregion
+}
