1371f2526c71d57e417854762ba6ddfffd82e247
f.settama <f.settama@isartdigital.com>
2015-02-07 18:59:34 +0100

    splashscreen

---
+++ UnityProject/Assets/Kintamagotchi/Scripts/SplashScreen.cs
@@ -0,0 +1,43 @@
+﻿//******************************************************************************
+// Authors: Frederic SETTAMA  
+//******************************************************************************
+
+using UnityEngine;
+using System.Collections;
+
+//******************************************************************************
+public class SplashScreen : MonoBehaviour 
+{
+#region Script Parameters
+	public float	Delay = 2;
+#endregion
+
+#region Static
+#endregion
+
+#region Properties
+#endregion
+
+#region Fields
+	// Const -------------------------------------------------------------------
+
+	// Private -----------------------------------------------------------------
+	private AsyncOperation	mAsync;
+	private float			mTime = 0;
+#endregion
+
+#region Unity Methods
+	void Start() 
+	{
+		
+	}
+
+	void Update () 
+	{
+		mTime += Time.deltaTime;
+		Debug.Log(mTime);
+		if (mTime > Delay)
+			Application.LoadLevel("main");
+	}
+#endregion
+}
