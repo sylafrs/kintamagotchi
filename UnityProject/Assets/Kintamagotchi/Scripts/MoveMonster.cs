b8fce90bc7b157cedb41b00ea8f6b6f1834be665
Blackhart <manceau.thomas19@gmail.com>
2015-02-07 10:25:48 +0100

    Rain plugin added + monster movement done

--- UnityProject/Assets/Kintamagotchi/Scripts/MoveMonster.cs
+++ UnityProject/Assets/Kintamagotchi/Scripts/MoveMonster.cs
@@ -1,10 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MoveMonster : MonoBehaviour {
+public class MoveMonster : MonoBehaviour 
+{
+    private Monster __monsterScript;
+
+    void    Start()
+    {
+        __monsterScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Monster>();
+    }
 
 	void OnInteraction(Vector3 point)
 	{
-		Monster.MoveTo(point);
+        __monsterScript.MoveTo(point);
 	}
 }
